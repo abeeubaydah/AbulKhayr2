69e06958b77a02748d3ddb7884a07f3ec6449fb4
Ogunsola Nurudeen Kolawole <56136093+abeeubaydah@users.noreply.github.com>
2020-03-24 17:00:20 +0100

    First_JS_Task
    My First Task Completed

---
+++ Script.js
@@ -0,0 +1,18 @@
+ let name = "Ogunsola Nurudeen Kolawole";
+ let course = "Frontend: All Courses, Backend: All Courses, Mobile: All Courses, Design: All Courses"
+ console.log("My name is: " + name);
+ console.log("Courses Offered are - " + course)
+ 
+ const data = [];
+ 
+ for (let i = 0; i <= 100; i++) {
+ 
+   if (i % 2 === 0) {
+ 
+     data.push(i);
+ 
+   }
+ 
+ }
+ 
+ console.log(data);
